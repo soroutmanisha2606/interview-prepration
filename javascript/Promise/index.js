bfc5a7eb40d33017db49614c33c82f94fd8389e6
manisha <soroutmanisha12@gmail>
2023-01-13 00:42:19 +0530

    adding promises with diffence in callbakhell and using promises

--- javascript/Promise/index.js
+++ javascript/Promise/index.js
@@ -1,9 +1,12 @@
 //koi cheez jo resolev ya reject hogi 
-//  or we can say that promise of code execution either it gfails or reject 
+//  or we can say that promise of code execution either it fails or reject 
 //it handles asyn operation in js
 //parally execution
 //3 state 
 // 1 . resolve    2.reject   3.pending
+//promise is immutable and resolve just once
+// a container for future value
+//a promise is an object that represents the completion of async operation
 
 let promise=new Promise(function(resolve,reject){
     console.log("pending state");
@@ -25,3 +28,22 @@ let promise=new Promise(function(resolve,reject){
         console.log(err);
     
     })
+
+
+
+
+    /////////////////////////////////////////////////////callback hell
+    const cart=["a","b","c"]
+    a(cart,function(id){
+        b(id,function(payment){
+            c(payment,function(successful){
+                d(successful)
+            })
+        })
+    })
+
+
+    ///////////////////promise
+    a(cart).then((id)=>b(id))
+    .then((payment)=>c(payment))
+    .then((successful)=>d(successful))
