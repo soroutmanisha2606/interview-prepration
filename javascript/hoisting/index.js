cb083bee9c80977795e5b5bb7aa3c7bb8281b645
Manisha Sorout <soroutmanisha12@gmail.com>
2023-01-21 23:27:49 +0530

    Update index.js

--- javascript/hoisting/index.js
+++ javascript/hoisting/index.js
@@ -1,7 +1,7 @@
 getname();
 console.log(getname)
 console.log(a);
-
+/////
 var a=10;
 //yha par hoisting kaam karega mtlb phele ek global execution context create hoga or usme 2 phases 
 //bannege 1 memory execution and dusra memory excution phase to jb hum phele declare karenge to memory execution phase mai
